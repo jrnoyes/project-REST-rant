b561af97aa337b19ea7f6920ac2722beb4f63eb7
Jason Noyes <jrnoyes1@gmail.com>
2022-07-25 20:33:55 -0500

    test

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,15 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/public/css/images/Beach.jpg" alt = "Beach" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@seefromthesky?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ishan @seefromthesky</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                <a href = "/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
     )
