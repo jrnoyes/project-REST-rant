71cb5be6a96c4589232916801e4f0032b35dc579
Jason Noyes <jrnoyes1@gmail.com>
2022-07-27 20:36:16 -0500

    New Places can be Added

--- models/places.js
+++ models/places.js
@@ -3,11 +3,11 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/beach.jpg'
+    pic: '/images/place1.jpg'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
+    pic: '/images/place2.jpg'
 }]
